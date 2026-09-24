1aa0a4deb1f7fe368299944b8ccddda9635abaf8
ilNick-03 <164178614+ilNick-03@users.noreply.github.com>
2026-09-24 23:31:24 +0200

    AETHER - Enable blur for Noctalia shell elements
    Added blur settings for Noctalia shell components including bar, panels, dock, and notifications.
    Please note Noctalia isn't still included in the AETHER experience.

--- ricing/Hyprland/AETHER/.config/hypr/modules/winrules.lua
+++ ricing/Hyprland/AETHER/.config/hypr/modules/winrules.lua
@@ -223,3 +223,27 @@ hl.window_rule({
     opacity          =   0.85,
     no_blur          =  false    -- Enable blur for a premium look
 })
+
+
+
+
+-- ===============================================================================
+-- [4]   NOCTALIA SHELL
+-- ===============================================================================
+
+--- Enable blur for Noctalia’s bar, panels, dock, and notifications.
+----  Just in case you want to switch from AETHER's Waybar to your customized Noctalia.
+----  Otherwise, ignore this code or delete it.
+------   See also:  https://docs.noctalia.dev/noctalia/compositor-settings/hyprland/
+
+hl.layer_rule({
+  name               =  "noctalia",
+  match = {
+    namespace        =  "^noctalia-(bar-.+|notification|dock|panel|attached-panel|osd|window-switcher)$",
+  },
+
+  no_anim            =  true,
+  ignore_alpha       =   0.5,
+  blur               =  true,
+  blur_popups        =  true,
+})
